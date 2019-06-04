ad947e0e27e83cf6b64c7663e9832f2c06a067bc
Morgan Caron <caron.morgan91@gmail.com>
2019-06-04 03:12:39 +0200

    Conversion of Router.js to Typescript

--- src/Router.ts
+++ src/Router.ts
@@ -1,33 +1,44 @@
-class Router {
-	constructor(mode = null) {
+interface Route {
+	regex: string
+	controller: () => void
+}
+
+class RouterController {
+	routes: Route[]
+	root: string
+	mode: string
+	currentFragment: string
+	interval: number
+
+	constructor(mode: string = undefined) {
 		this.routes = []
 		this.root = '/'
 		this.mode = mode || !!(history.pushState) ? 'history' : 'hash'
 		this.currentFragment = null
 	}
 
-	clearSlashes(path) {
+	clearSlashes(path: string): string {
 		return path.toString().replace(/\/$/, '').replace(/^\//, '')
 	}
 
-	getFragment() {
+	getFragment(): string {
 		let fragment = ''
 		if (this.mode === 'history') {
 			fragment = this.clearSlashes(decodeURI(location.pathname + location.search))
 			fragment = fragment.replace(/\?(.*)$/, '')
 			fragment = this.root != '/' ? fragment.replace(this.root, '') : fragment
 		} else {
-			const match = window.location.href.match(/#(.*)$/);
-			fragment = match ? match[1] : '';
+			const match = window.location.href.match(/#(.*)$/)
+			fragment = match ? match[1] : ''
 		}
 		return this.clearSlashes(fragment)
 	}
 
-	add(regex, func) {
-		this.routes.push({ regex: regex, function: func })
+	add(regex: string, controller: () => void): void {
+		this.routes.push({ regex: regex, controller: controller })
 	}
 
-	remove(regex) {
+	remove(regex: string): void {
 		for (let i = 0; i < this.routes.length; ++i) {
 			const route = this.routes[i]
 			if (regex.toString() === route.regex.toString())
@@ -35,35 +46,40 @@ class Router {
 		}
 	}
 
-	check(path = null) {
+	check(path: string = null): void {
 		const fragment = path || this.getFragment()
 		for (let i = 0; i < this.routes.length; ++i) {
 			const route = this.routes[i]
-			let match = fragment.match(route.regex)
+			let match = fragment.match('^' + route.regex + '$')
 			if (match) {
 				match.shift()
-				route.function.apply({}, match)
+				route.controller.apply({}, match)
 			}
 		}
 	}
 
-	listen() {
-		const func = () => {
+	listen(): void {
+		const updateRoute = () => {
 			if (this.currentFragment !== this.getFragment()) {
 				this.currentFragment = this.getFragment()
 				this.check(this.currentFragment)
 			}
 		}
 		clearInterval(this.interval)
-		this.interval = setInterval(func.bind(this), 50)
+		this.interval = window.setInterval(updateRoute.bind(this), 50)
 	}
 
-	navigate(path) {
+	navigate(path: string): void {
 		path = path ? path : '';
 		if (this.mode === 'history')
 			history.pushState(null, null, this.root + this.clearSlashes(path))
 		else
 			window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path
+		if (this.currentFragment !== this.getFragment()) {
+			this.currentFragment = this.getFragment()
+			this.check(this.currentFragment)
+		}
 	}
 }
-let router = new Router()
+
+export const Router = new RouterController()
