e40a2fe13502dd5285cf413b6e0adcf2f8425d20
Morgan Caron <caron.morgan91@gmail.com>
2019-06-05 04:14:42 +0200

    Adding menu

--- src/docs/ts/GettingStarted/Menu.ts
+++ src/docs/ts/GettingStarted/Menu.ts
@@ -10,6 +10,12 @@ export class Menu extends Layout {
 		return View`
 			ul {
 				li { a click: ${() => Router.navigate('a-simple-project')} "A Simple Project" }
+				li { a click: ${() => Router.navigate('component')} "A Component" }
+				li { a click: ${() => Router.navigate('view-syntax')} "The View Syntax" }
+				li { a click: ${() => Router.navigate('reactivity')} "The Reactivity" }
+				li { a click: ${() => Router.navigate('router')} "The Router" }
+				li { a click: ${() => Router.navigate('flip-animations')} "FLIP Animations" }
+				li { a click: ${() => Router.navigate('writer')} "The Writer" }
 			}
 		`
 	}
