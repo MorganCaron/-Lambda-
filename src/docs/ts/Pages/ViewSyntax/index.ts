807c5ff9014bc9cf355c9d5f9a523003ecc83a85
Morgan Caron <caron.morgan91@gmail.com>
2019-08-11 14:13:17 +0200

    Update documentation

--- src/docs/ts/Pages/ViewSyntax/index.ts
+++ src/docs/ts/Pages/ViewSyntax/index.ts
@@ -28,23 +28,24 @@ export class ViewSyntaxPage extends Layout {
 					code class: lang-scss "tag attribute: \\"...\\" { ... }" br;
 					
 				}
-				p class: lead {
-					strong "Empty HTML elements" " must end with a " strong "semicolon" br;
+				hr;
+				p class: lead { strong "Empty HTML elements" " must end with a " strong "semicolon" }
+				p class: text-center {
 					code class: lang-scss "br;" " for " code class: lang-html "<br />" br;
-					code class: lang-scss "input type: text;" ")." " for " code class: lang-html "<input type=\\"text\\" />" br;
+					code class: lang-scss "input type: text;" " for " code class: lang-html "<input type=\\"text\\" />" br;
 				}
-				p class: lead {
-					strong "Attributes" " are " strong "optional and cumulative" "." br;
+				p class: lead { strong "Attributes" " are " strong "optional and cumulative" "." }
+				p class: text-center {
 					code class: lang-scss "p \\"Text\\"" " for " code class: lang-html "<p>Text</p>" br;
 					code class: lang-scss "p class: Class1 \\"Text\\"" " for " code class: lang-html "<p class=\\"Class1\\">Text</p>"
 				}
-				p class: lead {
-					strong "Quotation marks (' or \\")" " enclosing the attribute value are " strong "optional if it contains only the characters" " present in this regex: " strong "[@_\-0-9A-Za-z]" "." br;
+				p class: lead { strong "Quotation marks (' or \\")" " enclosing the attribute value are " strong "optional if it contains only the characters" " present in this regex: " strong "[@_\-0-9A-Za-z]" "." }
+				p class: text-center {
 					code class: lang-scss "p class: Class1 \\"Text\\"" " for " code class: lang-html "<p class=\\"Class1\\">Text</p>" br;
 					code class: lang-scss "p class: \\"Class1 Class2\\" \\"Text\\"" " for " code class: lang-html "<p class=\\"Class1 Class2\\">Text</p>"
 				}
-				p class: lead {
-					"The " strong "braces enclosing the content" " of the tag are also " strong "optional if the tag contains only one element" "." br;
+				p class: lead { "The " strong "braces enclosing the content" " of the tag are also " strong "optional if the tag contains only one element" "." }
+				p class: text-center {
 					code class: lang-scss "p \\"Text\\"" " for " code class: lang-html "<p>Text</p>" br;
 					code class: lang-scss "p { \\"Line1\\" br; \\"Line2\\" }" " for " code class: lang-html "<p>Line1<br />Line2</p>" br;
 				}
