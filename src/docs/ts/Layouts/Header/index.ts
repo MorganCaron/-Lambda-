3612e1ed73ae3a2abe8237740ee5c8cb01139584
Morgan <caron.morgan91@gmail.com>
2020-04-23 00:06:02 +0200

    Header animation

--- src/docs/ts/Layouts/Header/index.ts
+++ src/docs/ts/Layouts/Header/index.ts
@@ -1,4 +1,4 @@
-import { Component, Reactive, Writer, WriterOptions } from 'ts/ModularDom'
+import { Component, Attribute, Writer, WriterOptions } from 'ts/ModularDom'
 import 'docs/ts/Layouts'
 
 import html from './index.html'
@@ -9,12 +9,24 @@ import html from './index.html'
 })
 export class Header extends HTMLElement {
 
+	@Attribute()
 	h1: string
-	reactive: Reactive<string>
-	writer: Writer
 
 	init() {
 		this.h1 = '<ModularDom/>'
+		const writerOptions: WriterOptions = {
+			interval: 100,
+			replace: true,
+			update: this.updateHeader.bind(this)
+		}
+		const writerHelloWorld = (oldString: string) => Writer.write(oldString, 'Hello World !', writerOptions, (newString: string) => setTimeout(writerVoid, 1000, newString))
+		const writerVoid = (oldString: string) => Writer.write(oldString, '</>', writerOptions, writerModularDom)
+		const writerModularDom = (oldString: string) => Writer.write(oldString, '<ModularDom/>', writerOptions, (newString: string) => setTimeout(writerHelloWorld, 5000, newString))
+		setTimeout(writerHelloWorld, 5000, this.h1)
+	}
+
+	updateHeader(newString: string) {
+		this.h1 = newString
 	}
 
 }
